0ebbc1ccbe14f8d00915817e421a12136e2e2db6
jessekops <645147@student.inholland.nl>
2023-06-26 20:51:41 +0200

    commit

--- backend-springboot/src/main/java/io/swagger/service/TransactionService.java
+++ backend-springboot/src/main/java/io/swagger/service/TransactionService.java
@@ -29,86 +29,61 @@ public class TransactionService {
 
     public Transaction createTransaction(Transaction trans) {
 
-        // User performing = owner account from
-
-        // Check if one of the accounts is of type savings
-        // If at least one account is a savings account continue savings transaction
         if (transactionValidatorService.checkCurrentOrSavings(accountService.findAccountByIban(trans.getFrom().getIban()), accountService.findAccountByIban(trans.getTo()))) {
-            // At least one account is a savings account
 
-            // Check if user is owner of both accounts
             if (!transactionValidatorService.isUserOwner(accountService.findAccountByIban(trans.getFrom().getIban()), accountService.findAccountByIban(trans.getTo()))) {
-                // User is not the owner of both accounts
+
                 throw new IllegalArgumentException("Cannot create transaction; cannot transfer from or to savings account that does not belong to you.");
+
             } else {
-                // Account from and account to might both be of type savings
                 checkGeneralConditions(trans); // Check if all conditions are met:
             }
-        } else { // Do transaction between two current accounts
+        } else {
             checkGeneralConditions(trans); // Check if all conditions are met
         }
-        // Update balance account from
+
         updateFromBalance(trans);
-        // Update balance account to
         updateToBalance(trans);
-        // Commit and save transaction
+
         return transactionRepo.save(trans);
     }
 
-    public Transaction createWithdrawal(Transaction trans) {
-        checkPinCode(trans);
-        checkGeneralConditions(trans);
-        // Update balance account from (Owner is user)
-        updateFromBalance(trans);
-        // Account to Owner is bank
-        // Commit and save transaction
-        return transactionRepo.save(trans);
-    }
 
-    public Transaction createDeposit(Transaction trans) {
-
-        // Get pin code (given by user) from transaction
-        Integer pinCode = trans.getPinCode();
-
-        // Check if pin code is filled in
-        if (pinCode == null) {
-            throw new IllegalArgumentException("Deposit failed; no pincode entered");
-        }
-
-        // Check if the given pin code matches the account pin code
-        if (!pinCode.equals(accountService.findAccountByIban(trans.getTo()).getPinCode())) {
-            throw new IllegalArgumentException("Deposit failed; wrong pin code entered");
-        }
-        // Account from Owner is bank
-        // Update balance account to (Owner is user)
-        updateToBalance(trans);
-        // Commit and save transaction
-        return transactionRepo.save(trans);
-    }
 
     private void checkGeneralConditions(Transaction trans) {
-        // Check if account from and account to are not the same account
+
         if (!transactionValidatorService.checkNotSameAccount(trans.getFrom().getIban(), trans.getTo())) {
-            // Account from is same as account to
+
             throw new IllegalArgumentException("Cannot create transaction; cannot transfer between two accounts that are the same.");
+
         } else {
-            // Check if both accounts are active
+
             if (!transactionValidatorService.checkActive(trans.getFrom().getIban(), trans.getTo())) {
+
                 throw new IllegalArgumentException("Cannot create transaction; at least one of the accounts is inactive.");
+
             } else {
-                // Check if given transaction amount is greater than 0
+
                 if (trans.getAmount() <= 0) {
+
                     throw new IllegalArgumentException("Cannot create transaction; Amount must be greater than 0.");
+
                 } else {
-                    // Check if absolute limit is exceeded
+
                     Account account = accountService.findAccountByIban(trans.getFrom().getIban());
+
                     if (!transactionValidatorService.checkAbsLimit(account, trans.getAmount())) {
+
                         throw new IllegalArgumentException("Cannot create transaction; Cannot exceed absolute limit " + "(" + account.getAbsLimit() + ").");
+
                     } else {
-                        // Check if day limit is exceeded
+
                         User userPerforming = userService.findById(trans.getUserPerforming());
+
                         if (!transactionValidatorService.checkDayLimit(userPerforming, trans)) {
+
                             throw new IllegalArgumentException("Cannot create transaction; Cannot exceed day limit " + "(" + userPerforming.getDayLimit() + ").");
+
                         }
                     }
                 }
@@ -116,35 +91,70 @@ public class TransactionService {
         }
     }
 
-    // Update account from
+
     private void updateFromBalance(Transaction trans) {
         Account account = accountService.findAccountByIban(trans.getFrom().getIban());
 
-        // Given that account exists
         account.setBalance((account.getBalance() - trans.getAmount()));
         accountRepo.save(account);
     }
 
-    // Update account to
+
+
     private void updateToBalance(Transaction trans) {
         Account account = accountService.findAccountByIban(trans.getTo());
 
-        // Given that account exists
         account.setBalance((account.getBalance() + trans.getAmount()));
         accountRepo.save(account);
     }
+
+    public Transaction createWithdrawal(Transaction trans) {
+        checkPinCode(trans);
+        checkGeneralConditions(trans);
+
+        updateFromBalance(trans);
+
+        return transactionRepo.save(trans);
+    }
+
+    public Transaction createDeposit(Transaction trans) {
+
+        Integer pinCode = trans.getPinCode();
+
+        if (pinCode == null) {
+            throw new IllegalArgumentException("Deposit failed; no pincode entered");
+        }
+
+        if (!pinCode.equals(accountService.findAccountByIban(trans.getTo()).getPinCode())) {
+            throw new IllegalArgumentException("Deposit failed; wrong pin code entered");
+        }
+
+        updateToBalance(trans);
+
+        return transactionRepo.save(trans);
+    }
+
 
     public Transaction findTransactionById(UUID id) {
         return transactionRepo.findTransactionById(id);
     }
 
+
+
+
     public void deleteTransaction(UUID id) {
         transactionRepo.delete(findTransactionById(id));
     }
 
+
+
+
     public Transaction updateTransaction(Transaction transaction) {
         return transactionRepo.save(transaction);
     }
+
+
+
 
     public List<Transaction> getTransactionsFromToday(LocalDate timeStamp) {
         return transactionRepo.findAllByTimestamp(timeStamp);
@@ -155,17 +165,20 @@ public class TransactionService {
         return pinCode == null;
     }
 
+
+
     private boolean isPinCodeCorrect(Transaction trans) {
         // Check if pin code is correct
         return trans.getPinCode().equals(accountService.findAccountByIban(trans.getFrom().getIban()).getPinCode());
     }
 
+
+
+
     private void checkPinCode(Transaction trans) {
-        // Get pin code (given by user) from transaction
         if (isPinCodeNull(trans.getPinCode())) {
             throw new IllegalArgumentException("Withdrawal failed; no pin code entered");
         }
-        // Check if the given pin code matches the account pin code
         if (!isPinCodeCorrect(trans)) {
             throw new IllegalArgumentException("Withdrawal failed; wrong pin code entered");
         }
--- backend-springboot/src/main/java/io/swagger/service/TransactionValidatorService.java
+++ backend-springboot/src/main/java/io/swagger/service/TransactionValidatorService.java
@@ -18,55 +18,70 @@ public class TransactionValidatorService {
     private AccountService accountService;
 
 
+
+
+
     // Method to check if it is current or savings account
 
     public boolean checkCurrentOrSavings(Account accountFrom, Account accountTo) {
         return isAccountSavings(accountFrom, accountTo);
-    }
-    // Method to check if the user is the owner of the account
-
-    public boolean isUserOwner(Account from, Account to) {
-        return isUserOwnerOfAccount(from, to);
-    }
-
-    // Method to check if the from account is not the same as to account
-
-    public boolean checkNotSameAccount(String accountFrom, String accountTo) {
-        return isFromSameAsTo(accountFrom, accountTo);
-    }
-
-    // Method to check if there is sufficient funds (absolute limit)
-
-    public boolean checkAbsLimit(Account accountFrom, double amount) {
-        return isAbsoluteLimitExceeded(accountFrom.getBalance(), amount, accountFrom.getAbsLimit());
-    }
-
-    // Method to check if it does not override day limit
-
-    public boolean checkDayLimit(User user, Transaction trans) {
-        return isDayLimitExceeded(user.getDayLimit(), trans);
-    }
-
-    // Method to check if both accounts are active
-
-    public boolean checkActive(String ibanFrom, String ibanTo) {
-        return areAccountsActive(accountService.findAccountByIban(ibanFrom), accountService.findAccountByIban(ibanTo));
     }
 
     private boolean isAccountSavings(Account from, Account to) {
         return from.getAccountType().equals(AccountType.SAVINGS) || to.getAccountType().equals(AccountType.SAVINGS);
     }
 
+
+
+
+
+    // Method to check if the user is the owner of the account
+
+    public boolean isUserOwner(Account from, Account to) {
+        return isUserOwnerOfAccount(from, to);
+    }
+
     private boolean isUserOwnerOfAccount(Account from, Account to) {
         return from.getUser().getId().equals(to.getUser().getId());
     }
 
+
+
+
+
+
+    // Method to check if the from account is not the same as to account
+
+    public boolean checkNotSameAccount(String accountFrom, String accountTo) {
+        return isFromSameAsTo(accountFrom, accountTo);
+    }
     private boolean isFromSameAsTo(String from, String to) {
         return !from.equals(to);
     }
 
+
+
+
+
+
+    // Method to check if there is sufficient funds (absolute limit)
+
+    public boolean checkAbsLimit(Account accountFrom, double amount) {
+        return isAbsoluteLimitExceeded(accountFrom.getBalance(), amount, accountFrom.getAbsLimit());
+    }
     private boolean isAbsoluteLimitExceeded(double balance, double amount, double absLimit) {
         return (balance - amount) > absLimit;
+    }
+
+
+
+
+
+
+    // Method to check if it does not override day limit
+
+    public boolean checkDayLimit(User user, Transaction trans) {
+        return isDayLimitExceeded(user.getDayLimit(), trans);
     }
 
     private boolean isDayLimitExceeded(Double dayLimit, Transaction trans) {
@@ -81,7 +96,21 @@ public class TransactionValidatorService {
         return total <= dayLimit;
     }
 
+
+
+
+
+
+    // Method to check if both accounts are active
+
+    public boolean checkActive(String ibanFrom, String ibanTo) {
+        return areAccountsActive(accountService.findAccountByIban(ibanFrom), accountService.findAccountByIban(ibanTo));
+    }
+
     private boolean areAccountsActive(Account from, Account to) {
         return from.getActive() && to.getActive();
     }
+
+
 }
+
